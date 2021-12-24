f8b60b33ef86422c1a413f956f19608accb65673
Manish V <imvmanish18@gmail.com>
2021-12-24 15:42:42 +0530

    production ready with some bugs

---
+++ social/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const format = require('date-format');
+
+const PORT = 3000 || process.env.PORT;
+
+app.get('/api/v1/:id',(req,res) => {
+    res.status(200).json({params: req.params.id});
+});
+
+app.get("/api/v1/instagram",(req,res) => {
+    const instaSocial = {
+        username: "imvmanish19",
+        followers: 2000,
+        following: 300,
+        date: format.asString("hh:mm:ss",new Date())
+    };
+    // res.status.send(`Username: <strong>${instaSocial.username}</strong><br>
+    // Followers: ${instaSocial.followers}<br>
+    // Following: ${instaSocial.following}
+    // `);
+    res.status(200).json({instaSocial});
+});
+
+app.get("/api/v1/facebook",(req,res) => {
+    const fbSocial = {
+        id: "imvmanish19",
+        followers: 2000,
+        following: 300,
+        date: format.asString("dd/MM hh:mm:ss",new Date())
+    };
+    // res.status.send(`Username: <strong>${fbSocial.username}</strong><br>
+    // Followers: ${fbSocial.followers}<br>
+    // Following: ${fbSocial.following}
+    // `);
+    res.status(200).json({fbSocial});
+});
+
+app.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}`);
+});
